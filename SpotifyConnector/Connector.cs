47c17ec4f28c27c69d0abae56169cd2dc3ffc5a5
Brandon Chong <brndnchong@gmail.com>
2018-03-21 22:46:39 -0700

    Add spotify connector

---
+++ SpotifyConnector/Connector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioBand.Connector;
+
+namespace SpotifyConnector
+{
+    public class Connector : IAudioConnector
+    {
+        public string ConnectorName { get; } = "Spotify";
+        public IObservable<int> TrackProgress { get; } = new SpotifyTrackProgress();
+
+        public void ChangeState(AudioState newAudioState)
+        {
+            TrackInfoChanged?.Invoke(this, new TrackInfoChangedEventArgs
+            {
+                TrackName = newAudioState.ToString()
+            });   
+        }
+
+        public void PreviousTrack()
+        {
+            TrackInfoChanged?.Invoke(this, new TrackInfoChangedEventArgs
+            {
+                TrackName = "previous track"
+            });
+        }
+
+        public void NextTrack()
+        {
+            TrackInfoChanged?.Invoke(this, new TrackInfoChangedEventArgs
+            {
+                TrackName = "next track"
+            });
+        }
+
+        public event EventHandler<TrackInfoChangedEventArgs> TrackInfoChanged;
+        public event EventHandler<AlbumArtChangedEventArgs> AlbumArtChanged;
+        public event EventHandler<AudioStateChangedEventArgs> AudioStateChanged;
+    }
+}
