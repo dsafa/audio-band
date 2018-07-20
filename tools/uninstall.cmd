7f151802208a84c153656f70f03a79a02f28b5d2
Brandon Chong <brndnchong@gmail.com>
2018-07-19 17:30:45 -0700

    Fix error if there is a space in the path

--- tools/uninstall.cmd
+++ tools/uninstall.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /unregister %~dp0AudioBand.dll
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /unregister "%~dp0AudioBand.dll"
 
 pause
