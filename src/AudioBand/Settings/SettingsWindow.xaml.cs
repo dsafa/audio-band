4ff8a18722040efafe3711c793eda6300f403b4d
Brandon Chong <brndnchong@gmail.com>
2018-08-28 22:26:34 -0700

    Add new settings window
    Basic layout
    
    Add layout for play pause button
    
    Finish layout for playback controls
    
    Add progress bar layout
    
    Add album art layout

---
+++ src/AudioBand/Settings/SettingsWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using MahApps.Metro.Controls;
+using NLog;
+
+namespace AudioBand.Settings
+{
+    public partial class SettingsWindow
+    {
+        private ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public SettingsWindow()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            InitializeComponent();
+        }
+
+        // Problem loading xceed.wpf.toolkit assembly normally
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (!args.Name.StartsWith("Xceed.Wpf.Toolkit"))
+            {
+                return null;
+            }
+
+            var dir = DirectoryHelper.BaseDirectory;
+            // name is in this format Xceed.Wpf.Toolkit, Version=3.4.0.0, Culture=neutral, PublicKeyToken=3e4669d2f30244f4
+            var asmName = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+            var filename = Path.Combine(dir, asmName);
+
+            return !File.Exists(filename) ? null : Assembly.LoadFrom(filename);
+        }
+    }
+}
