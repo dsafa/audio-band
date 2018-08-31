21e37a986608195ae90f7504d98b289adb5d881c
Brandon Chong <brndnchong@gmail.com>
2018-08-30 21:47:38 -0700

    Update models

---
+++ src/AudioBand/ViewModels/AudioSourceStatus.cs
@@ -0,0 +1,79 @@
+﻿using AudioBand.Annotations;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AudioBand.ViewModels
+{
+    internal class AudioSourceStatus : INotifyPropertyChanged
+    {
+        private bool _isPlaying;
+        private double _audioProgress;
+        private int _audioLength;
+        private string _songName;
+        private string _artist;
+
+        public bool IsPlaying
+        {
+            get => _isPlaying;
+            set
+            {
+                if (value == _isPlaying) return;
+                _isPlaying = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double AudioProgress
+        {
+            get => _audioProgress;
+            set
+            {
+                if (Math.Abs(value - _audioProgress) < 0.001) return;
+                _audioProgress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int AudioLength
+        {
+            get => _audioLength;
+            set
+            {
+                if (value == _audioLength) return;
+                _audioLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SongName
+        {
+            get => _songName;
+            set
+            {
+                if (value == _songName) return;
+                _songName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set
+            {
+                if (value == _artist) return;
+                _artist = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
