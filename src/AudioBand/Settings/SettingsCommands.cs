b73c9c94d79686e507176342ab5acb6b636626e6
Brandon Chong <brndnchong@gmail.com>
2018-09-09 22:11:33 -0700

    Add about audio band flyout

--- src/AudioBand/Settings/SettingsCommands.cs
+++ src/AudioBand/Settings/SettingsCommands.cs
@@ -4,9 +4,10 @@ namespace AudioBand.Settings
 {
     public static class SettingsCommands
     {
-        public static readonly RoutedCommand ResetImagePath = new RoutedCommand("Reset Path", typeof(SettingsCommands));
-        public static readonly RoutedCommand ChooseImage = new RoutedCommand("Chooose Image", typeof(SettingsCommands));
-        public static readonly RoutedCommand AddLabel = new RoutedCommand("Add Label", typeof(SettingsCommands));
-        public static readonly RoutedCommand DeleteLabel = new RoutedCommand("Delete Label", typeof(SettingsCommands));
+        public static readonly RoutedCommand ResetImagePath = new RoutedCommand(nameof(ResetImagePath), typeof(SettingsCommands));
+        public static readonly RoutedCommand ChooseImage = new RoutedCommand(nameof(ChooseImage), typeof(SettingsCommands));
+        public static readonly RoutedCommand AddLabel = new RoutedCommand(nameof(AddLabel), typeof(SettingsCommands));
+        public static readonly RoutedCommand DeleteLabel = new RoutedCommand(nameof(DeleteLabel), typeof(SettingsCommands));
+        public static readonly RoutedCommand ShowAbout = new RoutedCommand(nameof(ShowAbout), typeof(SettingsCommands));
     }
 }
