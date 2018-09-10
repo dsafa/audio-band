c778f265a3e77634d13b39f652f0193764967b2a
Brandon Chong <brndnchong@gmail.com>
2018-09-09 18:51:07 -0700

    Change label events to commands
    Add shortcut to add new text label using ctrl-n

--- src/AudioBand/Settings/SettingsCommands.cs
+++ src/AudioBand/Settings/SettingsCommands.cs
@@ -5,6 +5,8 @@ namespace AudioBand.Settings
     public static class SettingsCommands
     {
         public static readonly RoutedCommand ResetImagePath = new RoutedCommand("Reset Path", typeof(SettingsCommands));
-        public static readonly RoutedCommand ChooseImage = new RoutedCommand();
+        public static readonly RoutedCommand ChooseImage = new RoutedCommand("Chooose Image", typeof(SettingsCommands));
+        public static readonly RoutedCommand AddLabel = new RoutedCommand("Add Label", typeof(SettingsCommands));
+        public static readonly RoutedCommand DeleteLabel = new RoutedCommand("Delete Label", typeof(SettingsCommands));
     }
 }
