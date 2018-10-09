f1a14669c5db3a44d2162c4cbb415fe3478a8356
Brandon Chong <brndnchong@gmail.com>
2018-10-08 22:02:49 -0700

    Add classes to allow custom settings for audio sources

---
+++ src/AudioBand.AudioSource/AudioSourceSettingAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AudioBand.AudioSource
+{
+    /// <summary>
+    /// Expose this property as a setting that can be set by a user
+    /// </summary>
+    /// <inheritdoc cref="Attribute"/>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class AudioSourceSettingAttribute : Attribute
+    {
+        /// <summary>
+        /// Name that will be seen by user.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
