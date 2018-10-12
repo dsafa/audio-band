a8d37a953560f8cab7095fdffe3ee5d4b39ca1e9
Brandon Chong <brndnchong@gmail.com>
2018-10-11 19:05:12 -0700

    Add ui support to audio source settings

--- src/AudioBand/ViewModels/AudioSourceSettingsCollectionViewModel.cs
+++ src/AudioBand/ViewModels/AudioSourceSettingsCollectionViewModel.cs
@@ -1,15 +1,13 @@
-﻿using System;
+﻿using AudioBand.Annotations;
+using AudioBand.AudioSource;
+using AudioBand.Models;
+using NLog;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using AudioBand.Annotations;
-using AudioBand.AudioSource;
-using AudioBand.Models;
-using NLog;
 
 namespace AudioBand.ViewModels
 {
@@ -18,9 +16,14 @@ namespace AudioBand.ViewModels
         private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
         private Dictionary<string, AudioSourceSettingViewModel> _selectedSettings = new Dictionary<string, AudioSourceSettingViewModel>();
 
-        // audio source -> (setting name -> viewmodel)
+        /// <summary>
+        /// Mapping of settings of audiosource -> collection of (setting name, setting)
+        /// </summary>
         public Dictionary<string, Dictionary<string, AudioSourceSettingViewModel>> Settings { get; } = new Dictionary<string, Dictionary<string, AudioSourceSettingViewModel>>();
 
+        /// <summary>
+        /// The currently selected collection of audio source settings
+        /// </summary>
         public Dictionary<string, AudioSourceSettingViewModel> SelectedSettings
         {
             get => _selectedSettings;
@@ -36,57 +39,63 @@ namespace AudioBand.ViewModels
 
         public AudioSourceSettingsCollectionViewModel(List<IAudioSource> audioSources, List<AudioSourceSettingsCollection> audioSourceSettings)
         {
-            Merge(audioSources, audioSourceSettings);
+            CreateSettings(audioSources, audioSourceSettings);
         }
 
-        /// <summary>
-        /// Merge to update the audiosources values with ones that are saved
-        /// </summary>
-        private void Merge(List<IAudioSource> audioSources, List<AudioSourceSettingsCollection> savedSettings)
+        public List<AudioSourceSettingsCollection> ToModel()
+        {
+            return Settings.Select(s => new AudioSourceSettingsCollection
+                {
+                    Name = s.Key,
+                    Settings = s.Value.Values.Select(x => x.ToModel()).ToList()
+                }).ToList();
+        }
+
+        private void CreateSettings(List<IAudioSource> audioSources, List<AudioSourceSettingsCollection> savedSettings)
         {
             foreach (var audioSource in audioSources)
             {
-                var settings = GetExportedSettings(audioSource);
-                Settings.Add(audioSource.Name, settings);
-            }
+                var settings = GetExportedSettings(audioSource, savedSettings);
+                if (settings.Count == 0)
+                {
+                    continue;
+                }
 
-            foreach (var savedSetting in savedSettings)
-            {
-                if (Settings.TryGetValue(savedSetting.Name, out var settings))
-                {
-                   savedSetting.Settings.ForEach(s =>
-                   {
-                       if (settings.ContainsKey(s.Name))
-                       {
-                           settings[s.Name].Value = s.Value;
-                       }
-                       else
-                       {
-                           _logger.Warn($"Setting {s.Name} for audio source {savedSetting.Name} found but no matching setting found in the audio source");
-                       }
-                   });
-                }
-                else
-                {
-                    _logger.Warn($"Setting for audio source {savedSetting.Name} found but not matching audio source is found");
-                }
+                Settings.Add(audioSource.Name, settings);
             }
         }
 
         /// <summary>
         /// Look for properties marked with the setting attribute
         /// </summary>
-        private Dictionary<string, AudioSourceSettingViewModel> GetExportedSettings(IAudioSource audioSource)
+        private Dictionary<string, AudioSourceSettingViewModel> GetExportedSettings(IAudioSource audioSource, List<AudioSourceSettingsCollection> savedSettings)
         {
+            var saved = savedSettings.ToDictionary(s => s.Name, s => s.Settings.ToDictionary(s1 => s1.Name, s1 => s1));
+
             return audioSource.GetType().GetProperties()
                 .Select(property => new {Property = property, Attributes = property.GetCustomAttributes(typeof(AudioSourceSettingAttribute), true)})
                 .Where(p => p.Attributes.Length == 1)
                 .Select(p =>
                 {
-                    var attr = (AudioSourceSettingAttribute) p.Attributes[0];
-                    var s = new AudioSourceSetting() {Name = attr.Name};
-                    return new AudioSourceSettingViewModel(audioSource, p.Property, s);
+                        try
+                        {
+                            // Merge settings with those that are saved
+                            var attr = (AudioSourceSettingAttribute) p.Attributes[0];
+                            var type = p.Property.PropertyType;
+
+                            var value = saved.ContainsKey(audioSource.Name) && saved[audioSource.Name].ContainsKey(attr.Name)
+                                ? saved[audioSource.Name][attr.Name].Value 
+                                : p.Property.GetGetMethod(true).Invoke(audioSource, null)?.ToString();
+
+                            return new AudioSourceSettingViewModel(audioSource, p.Property, type, value, attr.Name);
+                        }
+                        catch (MissingMethodException)
+                        {
+                            _logger.Error($"Cannot create setting of type {p.Property.PropertyType}");
+                            return null;
+                        }
                 })
+                .Where(s => s != null)
                 .ToDictionary(s => s.Name, s => s);
         }
 
@@ -97,37 +106,47 @@ namespace AudioBand.ViewModels
         }
     }
 
-    // Holds additional information about the original source and property
     internal class AudioSourceSettingViewModel : INotifyPropertyChanged
     {
-        private readonly AudioSourceSetting _audioSourceSetting;
         private readonly PropertyInfo _propertyInfo;
         private readonly IAudioSource _audioSource;
         private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+        private readonly Type _valueType;
+        private object _value;
 
-        public string Name
-        {
-            get => _audioSourceSetting.Name;
-            set
-            {
-                _audioSourceSetting.Name = value;
-                OnPropertyChanged();
-            }
-        }
+        /// <summary>
+        /// Setting name
+        /// </summary>
+        public string Name { get; }
 
+        /// <summary>
+        /// Value of the setting, can be any basic type
+        /// </summary>
         public object Value
         {
-            get => _audioSourceSetting.Value;
+            get => _value;
             set
             {
-                // Check if value is valid
-                if (!_audioSource.ValidateSettingChange(Name, value))
+                if (Equals(value, _value))
                 {
                     return;
                 }
 
-                // Update the value in the audio source
-                _audioSourceSetting.Value = value;
+                if (value?.GetType() != _valueType)
+                {
+                    var converter = TypeDescriptor.GetConverter(value.GetType());
+                    if (converter.CanConvertTo(_valueType))
+                    {
+                        value = converter.ConvertTo(value, _valueType);
+                    }
+                    else
+                    {
+                        _logger.Warn($"Trying to set invalid type for setting {Name} - {value} {value?.GetType()}");
+                        return;
+                    }
+                }
+
+                _value = value;
 
                 try
                 {
@@ -144,11 +163,26 @@ namespace AudioBand.ViewModels
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public AudioSourceSettingViewModel(IAudioSource source, PropertyInfo propertyInfo, AudioSourceSetting setting)
+        public AudioSourceSettingViewModel(IAudioSource source, PropertyInfo propertyInfo, Type valueType, string valueString, string settingName)
         {
+            Name = settingName;
+            _valueType = valueType;
             _audioSource = source;
             _propertyInfo = propertyInfo;
-            _audioSourceSetting = setting;
+
+            try
+            {
+                _value = TypeDescriptor.GetConverter(valueType).ConvertFromString(valueString);
+            }
+            catch (Exception e)
+            {
+                _logger.Error($"Setting value does not match the type exported: {e}");
+            }
+        }
+
+        public AudioSourceSetting ToModel()
+        {
+            return new AudioSourceSetting(Name, _value.ToString());
         }
 
         [NotifyPropertyChangedInvocator]
