7bbb52e7d58453eb9059b13fb1c2f5e7c842a8b3
Brandon Chong <brndnchong@gmail.com>
2018-11-06 21:38:29 -0800

    Fix custom labels

--- src/AudioBand/MainControl.Bindings.cs
+++ src/AudioBand/MainControl.Bindings.cs
@@ -1,9 +1,13 @@
-﻿using AudioBand.ViewModels;
+﻿using System.Linq;
+using AudioBand.ViewModels;
+using AudioBand.Views.Winforms;
 
 namespace AudioBand
 {
     partial class MainControl
     {
+        private const string CustomLabelName = "CustomLabel";
+
         private void InitializeBindingSources(AlbumArtPopupVM albumArtPopupVm, AlbumArtVM albumartVm, AudioBandVM audioBandVm, NextButtonVM nextButtonVm,
             PlayPauseButtonVM playPauseButtonVm, PreviousButtonVM previousButtonVm, ProgressBarVM progressBarVm)
         {
@@ -14,6 +18,45 @@ namespace AudioBand
             PlayPauseButtonVMBindingSource.DataSource = playPauseButtonVm;
             PreviousButtonVMBindingSource.DataSource = previousButtonVm;
             ProgressBarVMBindingSource.DataSource = progressBarVm;
+        }
+
+        internal void AddCustomTextLabel(CustomLabelVM customLabel)
+        {
+            var label = new FormattedTextLabel(customLabel.FormatString, customLabel.Color, customLabel.FontSize, customLabel.FontFamily, customLabel.TextAlignment);
+            label.DataBindings.Add(nameof(label.Format), customLabel, nameof(customLabel.FormatString));
+            label.DataBindings.Add(nameof(label.DefaultColor), customLabel, nameof(customLabel.Color));
+            label.DataBindings.Add(nameof(label.FontSize), customLabel, nameof(customLabel.FontSize));
+            label.DataBindings.Add(nameof(label.FontFamily), customLabel, nameof(customLabel.FontFamily));
+            label.DataBindings.Add(nameof(label.Alignment), customLabel, nameof(customLabel.TextAlignment));
+            label.DataBindings.Add(nameof(label.Visible), customLabel, nameof(customLabel.IsVisible));
+            label.DataBindings.Add(nameof(label.Width), customLabel, nameof(customLabel.Width));
+            label.DataBindings.Add(nameof(label.Height), customLabel, nameof(customLabel.Height));
+            label.DataBindings.Add(nameof(label.Location), customLabel, nameof(customLabel.Location));
+            label.DataBindings.Add(nameof(label.ScrollSpeed), customLabel, nameof(customLabel.ScrollSpeed));
+
+            label.DataBindings.Add(nameof(label.AlbumName), _trackModel, nameof(_trackModel.AlbumName));
+            label.DataBindings.Add(nameof(label.Artist), _trackModel, nameof(_trackModel.Artist));
+            label.DataBindings.Add(nameof(label.SongName), _trackModel, nameof(_trackModel.TrackName));
+            label.DataBindings.Add(nameof(label.SongLength), _trackModel, nameof(_trackModel.TrackLength));
+            label.DataBindings.Add(nameof(label.SongProgress), _trackModel, nameof(_trackModel.TrackProgress));
+
+            label.Tag = customLabel;
+            Controls.Add(label);
+        }
+
+        internal void RemoveCustomTextLabel(CustomLabelVM customLabel)
+        {
+            var control = Controls.Find(CustomLabelName, true)
+                .Cast<FormattedTextLabel>()
+                .FirstOrDefault(l => l.Tag == customLabel);
+
+            if (control == null)
+            {
+                _logger.Warn("Tried removing label but the control was not found");
+                return;
+            }
+
+            Controls.Remove(control);
         }
     }
 }
--- src/AudioBand/ViewModels/CustomLabelsVM.cs
+++ src/AudioBand/ViewModels/CustomLabelsVM.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using AudioBand.Commands;
 using AudioBand.Models;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -7,13 +8,37 @@ namespace AudioBand.ViewModels
 {
     internal class CustomLabelsVM
     {
+        private readonly MainControl _control;
+
         public ObservableCollection<CustomLabelVM> CustomLabels { get; set; }
-        private MainControl _control;
+        public RelayCommand AddLabelCommand { get; }
+        public RelayCommand<CustomLabelVM> RemoveLabelCommand { get; }
 
         public CustomLabelsVM(List<CustomLabel> customLabels, MainControl control)
         {
             CustomLabels = new ObservableCollection<CustomLabelVM>(customLabels.Select(customLabel => new CustomLabelVM(customLabel)));
             _control = control;
+
+            foreach (var customLabelVm in CustomLabels)
+            {
+                _control.AddCustomTextLabel(customLabelVm);
+            }
+
+            AddLabelCommand = new RelayCommand(AddLabelCommandOnExecute);
+            RemoveLabelCommand = new RelayCommand<CustomLabelVM>(RemoveLabelCommandOnExecute);
+        }
+
+        private void AddLabelCommandOnExecute(object o)
+        {
+            var newLabel = new CustomLabelVM(new CustomLabel());
+            CustomLabels.Add(newLabel);
+            _control.AddCustomTextLabel(newLabel);
+        }
+
+        private void RemoveLabelCommandOnExecute(CustomLabelVM labelVm)
+        {
+            CustomLabels.Remove(labelVm);
+            _control.RemoveCustomTextLabel(labelVm);
         }
     }
 }
