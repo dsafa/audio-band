e8fdaf4a2af3fc31d9e7e71730f781ef497d39f9
Brandon Chong <brndnchong@gmail.com>
2018-11-07 20:45:41 -0800

    Create ISettings for settings

---
+++ src/AudioBand/Settings/Models/ISettings.cs
@@ -0,0 +1,13 @@
+﻿using AudioBand.Models;
+
+namespace AudioBand.Settings.Models
+{
+    internal interface ISettings
+    {
+        string Version { get; }
+
+        string AudioSource { get; set; }
+
+        T GetModel<T>();
+    }
+}
--- src/AudioBand/Settings/Models/v2/Settings.cs
+++ src/AudioBand/Settings/Models/v2/Settings.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using AudioBand.Models;
+using AutoMapper;
+using Nett;
 
 namespace AudioBand.Settings.Models.v2
 {
-    internal class Settings
+    internal class Settings : ISettings
     {
         public string Version { get; set; } = "2";
         public string AudioSource { get; set; }
@@ -15,5 +19,43 @@ namespace AudioBand.Settings.Models.v2
         public AlbumArtPopupSettings AlbumArtPopupSettings { get; set; }
         public List<CustomLabelSettings> CustomLabelSettings { get; set; }
         public List<AudioSourceSettings> AudioSourceSettings { get; set; }
+
+        private static readonly MapperConfiguration MapperConfig;
+
+        static Settings()
+        {
+            MapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<AlbumArtSettings, AlbumArt>();
+                cfg.CreateMap<AlbumArtPopupSettings, AlbumArtPopup>();
+                cfg.CreateMap<AudioBandSettings, AudioBand.Models.AudioBand>();
+                cfg.CreateMap<AudioBand.Settings.Models.v2.AudioSourceSetting, AudioBand.Models.AudioSourceSetting>();
+                cfg.CreateMap<AudioBand.Settings.Models.v2.AudioSourceSettings, AudioBand.Models.AudioSourceSettings>();
+                cfg.CreateMap<CustomLabelSettings, CustomLabel>();
+                cfg.CreateMap<NextButtonSettings, NextButton>();
+                cfg.CreateMap<PlayPauseButtonSettings, PlayPauseButton>();
+                cfg.CreateMap<PreviousButtonSettings, PreviousButton>();
+                cfg.CreateMap<ProgressBarSettings, ProgressBar>();
+            });
+        }
+
+        public T GetModel<T>()
+        {
+            var type = typeof(T);
+            object source;
+
+            if (type == typeof(AlbumArt)) source = AlbumArtSettings;
+            else if (type == typeof(AlbumArtPopup)) source = AlbumArtPopupSettings;
+            else if (type == typeof(AudioBandSettings)) source = AudioBandSettings;
+            else if (type == typeof(AudioBand.Models.AudioSourceSettings)) source = AudioSourceSettings;
+            else if (type == typeof(CustomLabel)) source = CustomLabelSettings;
+            else if (type == typeof(NextButton)) source = NextButtonSettings;
+            else if (type == typeof(PlayPauseButton)) source = PlayPauseButtonSettings;
+            else if (type == typeof(PreviousButton)) source = PreviousButtonSettings;
+            else if (type == typeof(ProgressBar)) source = ProgressBarSettings;
+            else throw new InvalidOperationException($"{type} could not be converted");
+
+            return MapperConfig.CreateMapper().Map<T>(source);
+        }
     }
 }
