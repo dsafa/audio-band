7ccab3e0436eb8f87ea688abb3d66a75c6047f1f
Brandon Chong <brndnchong@gmail.com>
2018-11-15 11:41:46 -0800

    Split into separate files and fix BeginEdit() being called many times

---
+++ src/AudioBand/Extensions/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace AudioBand.Extensions
+{
+    internal static class ObjectExtensions
+    {
+        public static string FormattedString(this object o)
+        {
+            var sb = new StringBuilder();
+            sb.Append("{ ");
+            foreach (var prop in o.GetType().GetProperties())
+            {
+                sb.Append($"{prop.Name}: {prop.GetValue(o)}, ");
+            }
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+    }
+}
