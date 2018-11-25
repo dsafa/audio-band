d4bd8d35955e4e380fe90e2c8ef6c1c5669a6153
Brandon Chong <brndnchong@gmail.com>
2018-11-24 22:22:19 -0800

    Add iTunes audio source

---
+++ src/iTunesAudioSource/AudioSource.cs
@@ -0,0 +1,157 @@
+﻿using AudioBand.AudioSource;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace iTunesAudioSource
+{
+    public class AudioSource : IAudioSource
+    {
+        private Timer _checkiTunesTimer;
+        private string _currentTrack;
+        private bool _isPlaying;
+        private ITunesControls _itunesControls = new ITunesControls();
+
+        public string Name => "iTunes";
+        public IAudioSourceLogger Logger { get; set; }
+        public event EventHandler<TrackInfoChangedEventArgs> TrackInfoChanged;
+        public event EventHandler TrackPlaying;
+        public event EventHandler TrackPaused;
+        public event EventHandler<TimeSpan> TrackProgressChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AudioSource()
+        {
+            _checkiTunesTimer = new Timer(100)
+            {
+                Enabled = false,
+                AutoReset = false
+            };
+            _checkiTunesTimer.Elapsed += CheckItunes;
+        }
+
+        public Task ActivateAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _itunesControls.Start();
+            _checkiTunesTimer.Start();
+            return Task.CompletedTask;
+        }
+
+        public Task DeactivateAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _checkiTunesTimer.Stop();
+            _itunesControls.Stop();
+
+            _isPlaying = false;
+            _currentTrack = null;
+
+            return Task.CompletedTask;
+        }
+
+        public Task NextTrackAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _itunesControls.Next();
+            return Task.CompletedTask;
+        }
+
+        public Task PauseTrackAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _itunesControls.Pause();
+            return Task.CompletedTask;
+        }
+
+        public Task PlayTrackAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _itunesControls.Play();
+            return Task.CompletedTask;
+        }
+
+        public Task PreviousTrackAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _itunesControls.Previous();
+            return Task.CompletedTask;
+        }
+
+
+        private void NotifyTrackChange(Track track)
+        {
+            var trackInfo = new TrackInfoChangedEventArgs
+            {
+                Artist = track.Artist,
+                Album = track.Album,
+                AlbumArt = track.Artwork,
+                TrackLength = track.Length,
+                TrackName = track.Name,
+            };
+
+            TrackInfoChanged?.Invoke(this, trackInfo);
+        }
+
+        private bool IsNewTrack(Track track)
+        {
+            var trackname = track.Artist + track.Name;
+            if (trackname == _currentTrack)
+            {
+                return false;
+            }
+
+            _currentTrack = trackname;
+            return true;
+        }
+
+        private void NotifyPlayerState()
+        {
+            var playing = _itunesControls.IsPlaying;
+            if (_isPlaying == playing)
+            {
+                return;
+            }
+
+            if (playing)
+            {
+                TrackPlaying?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                TrackPaused.Invoke(this, EventArgs.Empty);
+            }
+
+            _isPlaying = playing;
+        }
+
+        private void CheckItunes(object sender, ElapsedEventArgs eventArgs)
+        {
+            try
+            {
+                var track = _itunesControls.CurrentTrack;
+                if (track == null)
+                {
+                    return;
+                }
+
+                NotifyPlayerState();
+                var isNewTrack = IsNewTrack(track);
+                if (isNewTrack)
+                {
+                    NotifyTrackChange(track);
+                }
+
+                if (_isPlaying || isNewTrack)
+                {
+                    TrackProgressChanged?.Invoke(this, _itunesControls.Progress);
+                }
+            }
+            catch(Exception e)
+            {
+                Logger.Debug(e);
+            }
+            finally
+            {
+                _checkiTunesTimer.Enabled = true;
+            }
+        }
+    }
+}
