06774716419314957d360202441792fe59aa8b87
Brandon Chong <brndnchong@gmail.com>
2018-12-05 21:37:22 -0800

    Fix style for spotify audiosource

--- src/SpotifyAudioSource/SpotifyControls.cs
+++ src/SpotifyAudioSource/SpotifyControls.cs
@@ -1,43 +1,24 @@
-﻿using AudioBand.AudioSource;
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SpotifyAudioSource
 {
     // Controls and gets info from spotify. Implementation from rainmeter.
-    class SpotifyControls
+    public class SpotifyControls
     {
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        private static extern IntPtr FindWindowEx(IntPtr parent, IntPtr childAfter, string className, string windowName);
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern int GetWindowTextLength(IntPtr hWnd);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, IntPtr lParam);
-
         public const string SpotifyPausedWindowTitle = "Spotify";
-        private const int WM_APPCOMMAND = 0x0319;
         private const string SpotifyWindowClassName = "Chrome_WidgetWin_0"; // Last checked spotify version 1.0.88.353
         private IntPtr _spotifyHwnd;
 
-        private static string GetTitle(IntPtr hwnd)
-        {
-            var titleLength = GetWindowTextLength(hwnd);
-            var title = new StringBuilder(titleLength + 1);
-            GetWindowText(hwnd, title, title.Capacity);
-            
-            return title.ToString();
-        }
-
+        /// <summary>
+        /// Gets the title of the spotify window.
+        /// </summary>
+        /// <returns>The title of the spotify window. Null if not found.</returns>
         public string GetSpotifyWindowTitle()
         {
             // find spotify window by class name since the title changes depending on if a song is playing
-            var hwnd = FindWindowEx(IntPtr.Zero, IntPtr.Zero, SpotifyWindowClassName, null);
+            var hwnd = NativeMethods.FindWindowEx(IntPtr.Zero, IntPtr.Zero, SpotifyWindowClassName, null);
             if (hwnd == IntPtr.Zero)
             {
                 return null;
@@ -49,7 +30,7 @@ namespace SpotifyAudioSource
             while (GetTitle(hwnd).Length == 0)
             {
                 // At the top, this call should return null
-                var nextHwnd = FindWindowEx(IntPtr.Zero, hwnd, SpotifyWindowClassName, null);
+                var nextHwnd = NativeMethods.FindWindowEx(IntPtr.Zero, hwnd, SpotifyWindowClassName, null);
                 if (nextHwnd == IntPtr.Zero)
                 {
                     break;
@@ -69,7 +50,7 @@ namespace SpotifyAudioSource
                 return;
             }
 
-            SendMessage(_spotifyHwnd, WM_APPCOMMAND, 0, new IntPtr((int)AppCommand.Play));
+            NativeMethods.SendMessage(_spotifyHwnd, NativeMethods.WM_APPCOMMAND, 0, new IntPtr((int)NativeMethods.AppCommand.Play));
         }
 
         public void Pause()
@@ -79,7 +60,7 @@ namespace SpotifyAudioSource
                 return;
             }
 
-            SendMessage(_spotifyHwnd, WM_APPCOMMAND, 0, new IntPtr((int)AppCommand.Pause));
+            NativeMethods.SendMessage(_spotifyHwnd, NativeMethods.WM_APPCOMMAND, 0, new IntPtr((int)NativeMethods.AppCommand.Pause));
         }
 
         public void Previous()
@@ -89,7 +70,7 @@ namespace SpotifyAudioSource
                 return;
             }
 
-            SendMessage(_spotifyHwnd, WM_APPCOMMAND, 0, new IntPtr((int)AppCommand.Previous));
+            NativeMethods.SendMessage(_spotifyHwnd, NativeMethods.WM_APPCOMMAND, 0, new IntPtr((int)NativeMethods.AppCommand.Previous));
         }
 
         public void Next()
@@ -99,7 +80,7 @@ namespace SpotifyAudioSource
                 return;
             }
 
-            SendMessage(_spotifyHwnd, WM_APPCOMMAND, 0, new IntPtr((int)AppCommand.Next));
+            NativeMethods.SendMessage(_spotifyHwnd, NativeMethods.WM_APPCOMMAND, 0, new IntPtr((int)NativeMethods.AppCommand.Next));
         }
 
         public bool IsPaused()
@@ -107,12 +88,13 @@ namespace SpotifyAudioSource
             return GetSpotifyWindowTitle() == SpotifyPausedWindowTitle;
         }
 
-        private enum AppCommand
+        private static string GetTitle(IntPtr hwnd)
         {
-            Pause = 47 * 65536,
-            Play = 46 * 65536,
-            Next = 11 * 65536,
-            Previous = 12 * 65536,
+            var titleLength = NativeMethods.GetWindowTextLength(hwnd);
+            var title = new StringBuilder(titleLength + 1);
+            NativeMethods.GetWindowText(hwnd, title, title.Capacity);
+
+            return title.ToString();
         }
     }
 }
