55085748ad7d2aeb6517344d5ff68d246cfb6c97
Brandon Chong <brndnchong@gmail.com>
2018-12-05 21:04:43 -0800

    Fix style for audiosource project

--- src/AudioBand.AudioSource/IAudioSource.cs
+++ src/AudioBand.AudioSource/IAudioSource.cs
@@ -12,18 +12,6 @@ namespace AudioBand.AudioSource
     public interface IAudioSource
     {
         /// <summary>
-        /// Gets the name of the audio source.
-        /// </summary>
-        /// <value>The name of the audio source.</value>
-        string Name { get; }
-
-        /// <summary>
-        /// Gets or sets the <see cref="IAudioSourceLogger"/> used for logging.
-        /// </summary>
-        /// <value>Audio source logger that will be injected.</value>
-        IAudioSourceLogger Logger { get; set; }
-
-        /// <summary>
         /// Occurs when a setting has changed internally.
         /// </summary>
         event EventHandler<SettingChangedEventArgs> SettingChanged;
@@ -49,33 +37,57 @@ namespace AudioBand.AudioSource
         event EventHandler<TimeSpan> TrackProgressChanged;
 
         /// <summary>
-        /// Called when the audio source is active.
+        /// Gets the name of the audio source.
         /// </summary>
+        /// <value>The name of the audio source.</value>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="IAudioSourceLogger"/> used for logging.
+        /// </summary>
+        /// <value>Audio source logger that will be injected.</value>
+        IAudioSourceLogger Logger { get; set; }
+
+        /// <summary>
+        /// Called when the audio source becomes active.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The completed task.</returns>
         Task ActivateAsync(CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
         /// Called when the audio source is no longer active.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The completed task.</returns>
         Task DeactivateAsync(CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
         /// Called when there is a request to start playback.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The completed task.</returns>
         Task PlayTrackAsync(CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
         /// Called when there is a request to stop playback.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The completed task.</returns>
         Task PauseTrackAsync(CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
         /// Called when there is a request to skip to the previous track.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The completed task.</returns>
         Task PreviousTrackAsync(CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
         /// Called when there is a request to skip to the next track.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The completed task.</returns>
         Task NextTrackAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
---
+++ src/AudioBand.AudioSource/SettingOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AudioBand.AudioSource
+{
+    /// <summary>
+    /// Flags for audio source settings.
+    /// </summary>
+    [Flags]
+    public enum SettingOptions
+    {
+        /// <summary>
+        /// Setting is invisible to the user.
+        /// </summary>
+        Hidden = 1 << 0,
+
+        /// <summary>
+        /// Setting cannot be modified by the user.
+        /// </summary>
+        ReadOnly = 1 << 1,
+
+        /// <summary>
+        /// Indicates a sensitive setting such as a password, causing a warning to be given.
+        /// </summary>
+        Sensitive = 1 << 2,
+    }
+}
