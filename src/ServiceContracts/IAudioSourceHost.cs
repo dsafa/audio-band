d533bea68b2a031e5e3f192c7f0d66b4a05f86df
Brandon Chong <brndnchong@gmail.com>
2019-01-02 18:57:35 -0800

    Add wcf service contracts

---
+++ src/ServiceContracts/IAudioSourceHost.cs
@@ -0,0 +1,27 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    interface IAudioSourceHost
+    {
+        [OperationContract]
+        Task ActivateAsync();
+
+        [OperationContract]
+        Task DeactivateAsync();
+
+        [OperationContract]
+        Task PlayTrackAsync();
+
+        [OperationContract]
+        Task PauseTrackAsync();
+
+        [OperationContract]
+        Task PreviousTrackAsync();
+
+        [OperationContract]
+        Task NextTrackAsync();
+    }
+}
---
+++ src/ServiceContracts/ILoggingContract.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace ServiceContracts
+{
+    [ServiceContract]
+    public interface ILoggingContract
+    {
+        [OperationContract]
+        void Debug(string name, string message);
+
+        [OperationContract]
+        void Info(string name, string message);
+
+        [OperationContract]
+        void Warn(string name, string message);
+
+        [OperationContract]
+        void Error(string name, string message);
+    }
+}
