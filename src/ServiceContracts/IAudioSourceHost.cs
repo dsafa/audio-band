82b23ed866788c81c3aa9edd8ba5581669382022
Brandon Chong <brndnchong@gmail.com>
2019-01-03 23:06:27 -0800

    Clean up service contracts project

--- src/ServiceContracts/IAudioSourceHost.cs
+++ src/ServiceContracts/IAudioSourceHost.cs
@@ -1,29 +1,61 @@
 ﻿using System.ServiceModel;
 using System.Threading.Tasks;
+using AudioBand.AudioSource;
 
 namespace ServiceContracts
 {
-    [ServiceContract(CallbackContract = typeof(IAudioSourceHostCallback))]
+    /// <summary>
+    /// Contract for the service that is hosting the audio sources.
+    /// </summary>
+    [ServiceContract(Namespace = "Audioband", CallbackContract = typeof(IAudioSourceHostCallback))]
     public interface IAudioSourceHost
     {
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.Name"/>.
+        /// </summary>
+        /// <returns></returns>
         [OperationContract]
         string GetName();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.ActivateAsync(System.Threading.CancellationToken)"/>.
+        /// </summary>
+        /// <returns></returns>
         [OperationContract]
         Task ActivateAsync();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.DeactivateAsync(System.Threading.CancellationToken)"/>.
+        /// </summary>
+        /// <returns></returns>
         [OperationContract]
         Task DeactivateAsync();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.PlayTrackAsync(System.Threading.CancellationToken)"/>.
+        /// </summary>
+        /// <returns></returns>
         [OperationContract]
         Task PlayTrackAsync();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.PauseTrackAsync(System.Threading.CancellationToken)"/>.
+        /// </summary>
+        /// <returns></returns>
         [OperationContract]
         Task PauseTrackAsync();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.PreviousTrackAsync(System.Threading.CancellationToken)"/>.
+        /// </summary>
+        /// <returns></returns>
         [OperationContract]
         Task PreviousTrackAsync();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.NextTrackAsync(System.Threading.CancellationToken)"/>.
+        /// </summary>
+        /// <returns></returns>
         [OperationContract]
         Task NextTrackAsync();
     }
--- src/ServiceContracts/IAudioSourceHostCallback.cs
+++ src/ServiceContracts/IAudioSourceHostCallback.cs
@@ -4,20 +4,41 @@ using AudioBand.AudioSource;
 
 namespace ServiceContracts
 {
+    /// <summary>
+    /// Call back for <see cref="IAudioSourceHost"/>.
+    /// </summary>
     public interface IAudioSourceHostCallback
     {
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.SettingChanged"/>.
+        /// </summary>
+        /// <param name="args"></param>
         [OperationContract(IsOneWay = true)]
         void SettingChanged(SettingChangedEventArgs args);
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.TrackInfoChanged"/>.
+        /// </summary>
+        /// <param name="args"></param>
         [OperationContract(IsOneWay = true)]
         void TrackInfoChanged(TrackInfoChangedEventArgs args);
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.TrackPlaying"/>.
+        /// </summary>
         [OperationContract(IsOneWay = true)]
         void TrackPlaying();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.TrackPaused"/>.
+        /// </summary>
         [OperationContract(IsOneWay = true)]
         void TrackPaused();
 
+        /// <summary>
+        /// Contract method for <see cref="IAudioSource.TrackProgressChanged"/>.
+        /// </summary>
+        /// <param name="progress"></param>
         [OperationContract(IsOneWay = true)]
         void TrackProgressChanged(TimeSpan progress);
     }
