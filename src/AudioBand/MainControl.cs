0cb7659140f59dc6453612e391759890cbe4795b
Brandon Chong <brndnchong@gmail.com>
2019-02-10 22:08:41 -0800

    Remove debugger attach

--- src/AudioBand/MainControl.cs
+++ src/AudioBand/MainControl.cs
@@ -65,15 +65,13 @@ namespace AudioBand
         public MainControl()
         {
             InitializeComponent();
-#if DEBUG
-            System.Diagnostics.Debugger.Launch();
-#endif
+
             LogManager.ThrowExceptions = true;
             LogManager.Configuration = new XmlLoggingConfiguration(Path.Combine(DirectoryHelper.BaseDirectory, "nlog.config"));
             _uiDispatcher = Dispatcher.CurrentDispatcher;
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
+#pragma warning disable CS4014
             InitializeAsync();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
+#pragma warning restore CS4014
         }
 
         /// <summary>
