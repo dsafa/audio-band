7f20866a8ddb11ba9f56d7e8d32f56e3c23ec23a
Brandon Chong <brndnchong@gmail.com>
2019-04-26 17:19:18 -0700

    Add base class for next and previous button models

--- src/AudioBand/Models/NextButton.cs
+++ src/AudioBand/Models/NextButton.cs
@@ -3,67 +3,19 @@
     /// <summary>
     /// Model for the next button.
     /// </summary>
-    public class NextButton : ModelBase
+    public class NextButton : PlaybackButtonBase
     {
-        private string _imagePath = "";
-        private bool _isVisible = true;
-        private int _width = 73;
-        private int _height = 12;
-        private int _xPosition = 176;
-        private int _yPosition = 15;
-
         /// <summary>
-        /// Gets or sets the path of the button image.
+        /// Initializes a new instance of the <see cref="NextButton"/> class.
         /// </summary>
-        public string ImagePath
+        public NextButton()
         {
-            get => _imagePath;
-            set => SetProperty(ref _imagePath, value);
-        }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the button is visible.
-        /// </summary>
-        public bool IsVisible
-        {
-            get => _isVisible;
-            set => SetProperty(ref _isVisible, value);
-        }
-
-        /// <summary>
-        /// Gets or sets the width of the button.
-        /// </summary>
-        public int Width
-        {
-            get => _width;
-            set => SetProperty(ref _width, value);
-        }
-
-        /// <summary>
-        /// Gets or sets the height of the button.
-        /// </summary>
-        public int Height
-        {
-            get => _height;
-            set => SetProperty(ref _height, value);
-        }
-
-        /// <summary>
-        /// Gets or sets the x position of the button.
-        /// </summary>
-        public int XPosition
-        {
-            get => _xPosition;
-            set => SetProperty(ref _xPosition, value);
-        }
-
-        /// <summary>
-        /// Gets or sets the y position of the button.
-        /// </summary>
-        public int YPosition
-        {
-            get => _yPosition;
-            set => SetProperty(ref _yPosition, value);
+            ImagePath = "";
+            IsVisible = true;
+            Width = 73;
+            Height = 12;
+            XPosition = 176;
+            YPosition = 15;
         }
     }
 }
---
+++ src/AudioBand/Models/PlaybackButtonBase.cs
@@ -0,0 +1,69 @@
+﻿namespace AudioBand.Models
+{
+    /// <summary>
+    /// Base class for next and previous button
+    /// </summary>
+    public class PlaybackButtonBase : ModelBase
+    {
+        private string _imagePath;
+        private bool _isVisible;
+        private int _width;
+        private int _height;
+        private int _xPosition;
+        private int _yPosition;
+
+        /// <summary>
+        /// Gets or sets the path of the button image.
+        /// </summary>
+        public string ImagePath
+        {
+            get => _imagePath;
+            set => SetProperty(ref _imagePath, value);
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the button is visible.
+        /// </summary>
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set => SetProperty(ref _isVisible, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the button.
+        /// </summary>
+        public int Width
+        {
+            get => _width;
+            set => SetProperty(ref _width, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the height of the button.
+        /// </summary>
+        public int Height
+        {
+            get => _height;
+            set => SetProperty(ref _height, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the x position of the button.
+        /// </summary>
+        public int XPosition
+        {
+            get => _xPosition;
+            set => SetProperty(ref _xPosition, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the y position of the button.
+        /// </summary>
+        public int YPosition
+        {
+            get => _yPosition;
+            set => SetProperty(ref _yPosition, value);
+        }
+    }
+}
