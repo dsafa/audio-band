9708c2a5d033e1864dd9db1db2adc8a3d8685667
Brandon Chong <brndnchong@gmail.com>
2019-06-12 15:50:54 -0700

    Add remaining time text placeholder

---
+++ src/AudioBand/TextFormatting/RemainingTimePlaceholder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using AudioBand.AudioSource;
+using AudioBand.Extensions;
+
+namespace AudioBand.TextFormatting
+{
+    /// <summary>
+    /// A <see cref="TextPlaceholder"/> with the value of the remaining time in the song.
+    /// </summary>
+    public class RemainingTimePlaceholder : TextPlaceholder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RemainingTimePlaceholder"/> class.
+        /// </summary>
+        /// <param name="parameters">The placeholder parameters.</param>
+        /// <param name="audioSession">The audio session.</param>
+        public RemainingTimePlaceholder(IEnumerable<TextPlaceholderParameter> parameters, IAudioSession audioSession)
+            : base(parameters, audioSession)
+        {
+            AddSessionPropertyFilter(nameof(IAudioSession.SongProgress));
+            AddSessionPropertyFilter(nameof(IAudioSession.SongLength));
+        }
+
+        /// <inheritdoc />
+        public override string GetText()
+        {
+            if (Session.SongLength < Session.SongProgress)
+            {
+                return TimeSpan.Zero.Format();
+            }
+
+            return (Session.SongLength - Session.SongProgress).Format();
+        }
+
+        /// <inheritdoc />
+        protected override void OnAudioSessionPropertyChanged(string propertyName)
+        {
+            RaiseTextChanged();
+        }
+    }
+}
